1cb094a7c9d835209d451553dad514bf527f1be7
Unknown <you@example.com>
2020-05-21 15:08:22 +0200

    Otwieranie okna programu.

--- Generator.py
+++ Generator.py
@@ -1,5 +1,18 @@
 def main():
-    pass
+    import pygame
+
+    pygame.init()
+
+    white = (255, 255, 255)
+
+    display = pygame.display.set_mode((800, 600))
+    display.fill(white)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
 if __name__ == '__main__':
     main()
